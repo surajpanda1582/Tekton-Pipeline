d8fc658b586824cce21bf34b572034b0648f612d
surajpanda1582 <37118340+surajpanda1582@users.noreply.github.com>
2020-08-13 14:47:24 +0530

    Create CreatePipeline.sh

---
+++ CreatePipeline.sh
@@ -0,0 +1,27 @@
+git clone https://github.com/ibm-garage-cloud/cloud-shell-commands.git
+export PATH=~/cloud-shell-commands:$PATH
+source ./cloud-shell-commands/install-igc
+oc sync --version
+git config --global user.email "panda.suraj@gmail.com"
+git config --global user.name "surajpanda1582"
+
+git clone https://github.com/springbootMicroservice/Microservice.git
+
+cd Microservices
+
+oc enable
+
+read 'y'
+read 'y'
+
+git add .
+git commit -m "update chart"
+git push origin master
+
+oc sync chatbotdeplypipeline --dev
+
+oc pipeline 
+
+read tekton
+
+read igc-maven
